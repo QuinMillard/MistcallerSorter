a124d9ebd482ed5193f8353b61496d8025bcc6b4
Jordan Verasamy <j.verasamy@gmail.com>
2018-11-03 09:31:21 -0400

    some more stuff

--- bullshit.py
+++ bullshit.py
@@ -1,9 +1,17 @@
 import pprint
 
-def read_files(file_name):
+FILENAME = 'deck_1.csv'
+
+def raw_data_from(file_name):
     f = open(file_name, 'r')
     x = f.read().splitlines()
     f.close()
     return x
 
-pprint.pprint(read_files('deck_1.csv'))
+def deck_from(lst):
+    return { ' '.join(a.split()[1:]) : int(a.split()[0]) for a in lst }
+
+
+raw_data = raw_data_from(FILENAME)
+deck = deck_from(raw_data)
+pprint.pprint(deck)
