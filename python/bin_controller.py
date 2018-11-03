c8e73b0a57e74111e64e6aec34fa8800c8b3a0ff
Jordan Verasamy <j.verasamy@gmail.com>
2018-11-03 15:50:07 -0400

    introduce bin controller (#4)

---
+++ python/bin_controller.py
@@ -0,0 +1,25 @@
+import serial
+import time
+
+class BinController:
+    def connect(self):
+        self.ser = serial.Serial('/dev/cu.wchusbserial14520', 9600)
+        time.sleep(2) # ugly, but required to avoid race condition
+
+    def close(self):
+        self.ser.close()
+
+    def place_in_bin_1(self):
+        self.ser.write(b'1')
+
+    def place_in_bin_2(self):
+        self.ser.write(b'2')
+
+    def place_in_bin_3(self):
+        self.ser.write(b'3')
+
+    def place_in_bin_4(self):
+        self.ser.write(b'4')
+
+    def place_in_bin_5(self):
+        self.ser.write(b'5')
