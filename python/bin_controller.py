c15311ab58e5f618bf2d72664e65b29712cdaf26
Jordan Verasamy <j.verasamy@gmail.com>
2018-11-03 17:19:27 -0400

    add rule and bin controller logic so we can figure out how to allocate things to the right bin

--- python/bin_controller.py
+++ python/bin_controller.py
@@ -3,23 +3,42 @@ import time
 
 class BinController:
     def connect(self):
-        self.ser = serial.Serial('/dev/cu.wchusbserial14520', 9600)
-        time.sleep(2) # ugly, but required to avoid race condition
+        print("[INFO] Opening serial connection.")
+        #self.ser = serial.Serial('/dev/cu.wchusbserial14520', 9600)
+        #time.sleep(2) # ugly, but required to avoid race condition
 
     def close(self):
-        self.ser.close()
+        print("[INFO] Closing serial connection.")
+        #self.ser.close()
+
+    def place_in_bin(self, bin_number):
+        switcher = {
+            1: self.place_in_bin_1,
+            2: self.place_in_bin_2,
+            3: self.place_in_bin_3,
+            4: self.place_in_bin_4,
+            5: self.place_in_bin_5,
+        }
+        switcher.get(bin_number, "Invalid method")()
+
+    ## -----------------------------------------------
 
     def place_in_bin_1(self):
-        self.ser.write(b'1')
+        #self.ser.write(b'1')
+        return None
 
     def place_in_bin_2(self):
-        self.ser.write(b'2')
+        #self.ser.write(b'2')
+        return None
 
     def place_in_bin_3(self):
-        self.ser.write(b'3')
+        #self.ser.write(b'3')
+        return None
 
     def place_in_bin_4(self):
-        self.ser.write(b'4')
+        #self.ser.write(b'4')
+        return None
 
     def place_in_bin_5(self):
-        self.ser.write(b'5')
+        #self.ser.write(b'5')
+        return None
