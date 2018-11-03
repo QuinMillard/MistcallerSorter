f84672a3ece70e9df9ee5daab78ad1ca79c2393d
Jordan Verasamy <j.verasamy@gmail.com>
2018-11-03 18:41:36 -0400

    fuzzy matching and retries (#6)

--- python/card.py
+++ python/card.py
@@ -1,6 +1,10 @@
 import json
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 class Card:
+    FUZZY_MATCH_THRESHOLD = 90
+
     with open('../json/card_color_identities.json', 'r') as f:
         print("[INFO] Loading card data...")
         card_data = json.load(f)
@@ -8,12 +12,14 @@ class Card:
 
     @classmethod
     def lookup(klass, names):
+        max_score = klass.FUZZY_MATCH_THRESHOLD
+        current_best_card = None
         for name in names:
-            try:
-                color_identity = klass.card_data[name]
-                return klass(name, color_identity)
-            except KeyError:
-                continue
+            fuzzy_matched_name, score = process.extractOne(name, list(klass.card_data.keys()))
+            #print("--- BEST MATCH: {}, SCORE: {} ---".format(fuzzy_matched_name, score))
+            if score > max_score:
+                current_best_card = Card(fuzzy_matched_name, klass.card_data[fuzzy_matched_name])
+        return current_best_card
 
     def __init__(self, name, color_identity):
         self.name = name
